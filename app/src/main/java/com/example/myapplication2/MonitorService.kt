5b946fee21d3f87d97d0f390cd7b7cd40954ddbf
Anton Kojin <antonkojin@gmail.com>
2026-04-20 15:57:11 +0200

    refresh button in notification

--- app/src/main/java/com/example/myapplication2/MonitorService.kt
+++ app/src/main/java/com/example/myapplication2/MonitorService.kt
@@ -152,6 +152,7 @@ class MonitorService : Service() {
             .setSmallIcon(R.drawable.ic_notification)
             .setContentIntent(intent(null))
             .setPriority(NotificationCompat.PRIORITY_LOW).setSilent(true).setOngoing(true)
+            .addAction(0, "Refresh", intent(ACTION_REFRESH))
             .addAction(0, "Stop Monitoring", intent(ACTION_STOP_SERVICE))
             .apply {
                 if (bigText != null) setStyle(
